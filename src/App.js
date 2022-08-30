4a2d429f56b9f007b59a6b39f648e54d380711e7
Anubha Pant <pant.anubha24@gmail.com>
2022-08-30 13:26:52 +0530

    useReducer implementation

--- src/App.js
+++ src/App.js
@@ -1,82 +1,15 @@
-import { getConfig } from "@testing-library/react";
-import { useEffect, useState } from "react";
-import MultiselectCheckbox from "./MultiSelectCheckbox";
+import Post from "./Post";
+
 function App() {
-  const options = [
-    { label: "Item One" },
-    { label: "Item Two" },
-    { label: "Item Three" },
-    { label: "Item Four" },
-  ];
-  const [isPressed, setIsPressed] = useState(false);
-  const [bitcoinPrice, setBitcoinPrice] = useState([]);
-  const getBitcoin = async () => {
-    try {
-      const call = await fetch(
-        "https://api.coinbase.com/v2/prices/BTC-USD/buy",
-        {
-          method: "GET",
-          authorization:
-            "Bearer abd90df5f27a7b170cd775abf89d632b350b7c1c9d53e08b340cd9832ce52c2c",
-        }
-      );
-      const response = await call.json();
-
-      return await response;
-    } catch {
-      return [];
-    }
-  };
-
-  const setAmount = async () => {
-    const data = await getBitcoin();
-
-    if (!data) return;
-
-    console.log(data);
-    // {"data":{"base":"BTC","currency":"USD","amount":"19891.09"}}
-
-    const {
-      data: { amount },
-    } = data;
-
-    // New amount is pushed in front of the old amounts that have been deconstructed.
-    // We then slice the array at 2 so we get rid of any old data we don't want to use anymore
-    const newPrices = [amount, ...bitcoinPrice].slice(2);
-    console.log("new peice", newPrices);
-    setBitcoinPrice(newPrices);
-  };
-
-  useEffect(() => {
-    console.log("here is bitcoin price", bitcoinPrice);
-  }, [bitcoinPrice]);
-
   return (
-    <div>
-      {/* <MultiselectCheckbox
-        options={options}
-        onChange={(data) => {
-          console.log(data);
-        }}
-      /> */}
-      <button
-        onClick={() => {
-          setIsPressed(!isPressed);
-          setAmount();
-        }}
-      >
-        {isPressed ? "refresh Bitcoin price" : "get Bitcoin price"}
-      </button>
-      <div>
-        <div>
-          {bitcoinPrice.map((value, i) => (
-            <div key={i}>
-              {i === 0 ? "Current price: " : "Previous price: "}{" "}
-              <h1>{value.amount}</h1>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Post />
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from "react";
+import { INITIAL_STATE, postReducer } from "./postReducer";
+
+const Post = () => {
+  const [state, dispatch] = useReducer(postReducer, INITIAL_STATE);
+  const handleFetch = () => {
+    dispatch({ type: "FETCH_START" });
+    fetch("")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: "FETCH_ERROR" });
+      });
+  };
+  return (
+    <div>
+      <button style={{ marginTop: "20px" }} onClick={handleFetch}>
+        {state.loading ? "Wait..." : "Fetch the Post"}
+      </button>
+
+      <p>{state?.post?.title}</p>
+      <span>{state.error && "Something went Wrong!"}</span>
+    </div>
+  );
+};
+
+export default Post;
